4752ebff277afbc081b4301903a7235ed179b7fa
johny-on-git <49092647+johny-on-git@users.noreply.github.com>
2025-07-01 21:30:22 +0530

    Time: 2 ms (98.97%), Space: 45.1 MB (46.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,73 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        int[] returnX = new int[2];
-        Boolean found = false;
-
-        for(int i=0; i < nums.length-1; i++)
-        {
-            for(int j=1; j <nums.length; j++)
-            {   
-               
-            
-                 if( i!=j && nums[i]>=0 && nums[j]>=0  && (! (nums[i] > target) || !  (nums[j] > target) ) )
-                 {
-                if(nums[i] + nums[j] == target )
-                {
-                        returnX[0] = i;
-                        returnX[1] = j;
-                       found = true;
-                       break;
-
-                }
-                 }
-
-                 if( i!=j &&  ( nums[i]<=0 || nums[j]<=0 ) )
-                 {  
-                     System.out.println("Comaring" + i + " ," + j);
-                        if( nums[i]<=0 && nums[j]<=0)
-                        {
-                            int x =  nums[i] * (-1);
-                            int y =  nums[j] * (-1);
-                            if(x+y == target*(-1)) 
-                            {
-                                returnX[0] = i;
-                                returnX[1] = j;
-                                found = true;
-                                break;
-                            }
-                        }
-
-                        else
-                        {
-                            if( nums[i] > nums[j])
-                            {
-                                if( nums[i] + nums[j] == target) 
-                            {
-                                returnX[0] = i;
-                                returnX[1] = j;
-                                found = true;
-                                break;
-                            }
-                            }
-                            else{
-                                 if( nums[j] + nums[i] == target) 
-                            {
-                                returnX[0] = i;
-                                returnX[1] = j;
-                                found = true;
-                                break;
-                            }
-                            }
-                        }
-                 }
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
             }
-            if (found)
-            {break;}
+            map.put(nums[i],i);
         }
-
-
-        return returnX;
+        return new int[0];
         
     }
 }
