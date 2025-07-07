71cfcc79f017e0fef2373d43796faa5c6744e7a0
johny-on-git <49092647+johny-on-git@users.noreply.github.com>
2025-07-07 09:12:34 +0530

    Time: 14 ms (6.33%), Space: 44.7 MB (5.03%) - LeetHub

---
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findLucky(int[] arr) {
+
+Map<Integer,Integer> f = new HashMap<Integer,Integer>();
+     for(int i : arr)
+     {
+        f.put(i,f.getOrDefault(i,0)+1);
+     }
+
+     System.out.println(f);
+     List<Integer> luckyValues = f.entrySet().stream().filter(entry -> entry.getKey().equals(entry.getValue()))
+                                    .map(Map.Entry::getKey).collect(Collectors.toList());
+
+            System.out.println(luckyValues.toString());
+
+            return luckyValues.stream().max(Integer::compareTo).orElse(-1);
+
+    }
+}
