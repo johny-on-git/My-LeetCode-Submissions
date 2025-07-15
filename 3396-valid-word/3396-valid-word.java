17e8f09fca2358a6ba4637120784a139be5068f8
johny-on-git <49092647+johny-on-git@users.noreply.github.com>
2025-07-15 10:13:51 +0530

    Time: 1 ms (99.12%), Space: 42.3 MB (26.49%) - LeetHub

--- 3396-valid-word/3396-valid-word.java
+++ 3396-valid-word/3396-valid-word.java
@@ -1,15 +1,51 @@
 class Solution {
     public boolean isValid(String word) {
 
-        //Boolean check1 = false;
-        //Boolean check2 = false;
-        //Boolean check3 = false;
-        //Boolean check4 = false;
+        Boolean check1 = (word.length() >= 3);
+        Boolean check2 = true;
+        Boolean check3 = false;
+        Boolean check4 = false;
 
-        return (word.length() >= 3) 
+        for(Character c : word.toCharArray())
+        {   
+            int ascii = (int) c;
+            if( (ascii >= 48 && ascii <= 57)  || (ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122) )
+            {
+                if(check3 == false)
+                {
+                    if( c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                       || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' )
+                    {
+                        check3 = true;
+                    }
+                }
+
+                if(check4 == false)
+                {
+                    if( !(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                       || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ) 
+                       && 
+                       !(ascii >= 48 && ascii <= 57)
+                       )
+                    {
+                        check4 = true;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return check1 && check2 && check3 && check4;
+
+       /* return (word.length() >= 3) 
         && word.matches("[a-zA-Z0-9]+") 
         && word.matches(".*[aeiouAEIOU].*") 
-        && word.matches(".*[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ].*");
+        && word.matches(".*[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ].*"); */
+
+
         
     }
 }
